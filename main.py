a36afc0d67c775fa1ca661e730dbbf741393da71
User <calebwilkerson31@gmail.com>
2026-04-08 11:36:26 -0500

    helper commit

--- main.py
+++ main.py
@@ -1,13 +1,61 @@
-
+import data
+import helpers
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class TestUrbanRoutes:
+
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route")
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number")
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for fill card")
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for blanket and handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Loop to simulate ordering 2 ice creams
+        for i in range(2):
+            # Add in S8
+            pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for car search model appears")
+        pass
